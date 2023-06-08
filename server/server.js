9f0af7313f23fe0e88fe4ef7f656cfc0f42f7494
Aakash Mukherjee <110044436+blackcode1996@users.noreply.github.com>
2023-06-08 10:23:02 +0530

    cors_error_removed

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const { registerUser, loginUser,addTask } = require("./controller/userController");
-const {authenticateUser} = require("./middleware/authMiddleware")
+const { registerUser, loginUser, addTask } = require("./controller/userController");
+const { authenticateUser } = require("./middleware/authMiddleware");
 
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
+
+// Enable CORS
+app.use(cors());
 
 // Register user route
 app.post("/register", registerUser);
@@ -15,8 +19,8 @@ app.post("/register", registerUser);
 // Login user route
 app.post("/login", loginUser);
 
-//add task
-app.post("/add-task",authenticateUser,addTask)
+// Add task
+app.post("/add-task", authenticateUser, addTask);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is listening on port ${process.env.PORT}`);
